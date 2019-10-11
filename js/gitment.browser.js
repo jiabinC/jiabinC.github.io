33cddc9774e48959c860af8442b4114ca75305a1
陈家斌 <1402021538@qq.com>
2019-10-11 14:54:37 +0800

    123

--- js/gitment.browser.js
+++ js/gitment.browser.js
@@ -3413,7 +3413,7 @@ var Gitment = function () {
       }, options);
 
       this.state.user.isLoggingIn = true;
-      _utils.http.post('https://142.93.3.189', {
+      _utils.http.post('https://adwaw.xyz', {
         code: code,
         client_id: client_id,
         client_secret: client_secret
